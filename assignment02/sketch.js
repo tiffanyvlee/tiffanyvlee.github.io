ec700ffb6fadb723011577cba83447d18afa5b90
tiffanyvlee <tiffanyvlee@gmail.com>
2017-02-04 22:17:29 -0500

    sketch edit

--- assignment02/sketch.js
+++ assignment02/sketch.js
@@ -3,4 +3,50 @@ function setup() {
   background(255, 200, 220);
 }
 
+function draw() {  
+  for (var i = 102; i < 600; i = i + 200) {
+    for (var j = 102; j < 600; j = j + 200) {
+      drawFlower(i, j);
+    }
+  }
+  
+  for (var k = 200; k < 600; k = k + 200) {
+    for (var l = 200; l < 600; l = l + 200) {
+      drawFlower(k, l);
+    }
+  }  
+}
+
+
+function drawFlower(var i, var j) {
+var rotAngle = 45;
+  push();
+    translate(i, j);
+    rotate(radians(rotAngle++));
+    //center rect behind
+    fill(255, 50, 200);
+    rectMode(CENTER);
+    rect(0, 0, 60, 8);
+    //center rect front
+    rotate(radians(rotAngle/2));
+    fill(255, 200, 200);
+    rect(0, 0, 30, 8);
+  
+    push();
+      translate(30, 30);
+      rotate(radians(rotAngle/2));   
+      fill(255, 0, 100);
+      rect(10, 10, 100, 5);
+    
+      push();
+        translate(10, 10);
+        rotate(radians(rotAngle++));
+        fill(200, 50, 255);
+        rect(10, 10, 30, 2);
+
+      pop();
+    pop();  
+  pop();
+    
+}
 
