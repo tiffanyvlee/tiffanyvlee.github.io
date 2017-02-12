85ff484ca654b71867b3b6934b7115c7b0721ff6
tiffanyvlee <tiffanyvlee@gmail.com>
2017-02-11 22:25:02 -0500

    test

--- assignment03/sketch.js
+++ assignment03/sketch.js
@@ -1,6 +1,28 @@
 var textbox;
 var slider;
 var paragraph;
+var circles;
+
+function circle(x, y) {
+  this.x = x;
+  this.y = y;
+  this.col = color (map(mouseX, 0, 600, 0, 255));
+
+  this.display = function() {
+    noStroke();
+    fill(this.col);
+    ellipse(this.x, this.y, 50, 50);
+  }
+
+  this.move = function() {
+    this.x += random(-1, 1);
+    this.y += random(-1, 1);
+  }
+
+  this.clicked = function() {
+    this.col = color(0, 0, 255);
+  }
+}
 
 function setup() {
   paragraph = createP("text");
@@ -18,12 +40,36 @@ function setup() {
   bgcolor = color(200);
 
   canvas.mouseOver(changeColor);
+
+  for (var i = 0; i < 1; i++) {
+    circles[i] = new circle(300, 300);
+  }
 }
 
 function draw() {
   background(bgcolor);
   fill(map(mouseX, 0, 600, 0, 255));
-  rect(100, 100, 100, 100);
+  ellipse(100, 100, 100, 100);
+
+  for (var i = 0; i < circles.length; i++) {
+    circles[i].display();
+    circles[i].move();
+  }
+  if (circles.length > 50){
+    circles.splice(0, 1);
+  }
+}
+
+function mousePressed() {
+  for (var i = 0; i < circles.length; i++) {
+    if (dist(mouseX, mouseY, circles[i].x, circles[i].y) < 20) {
+      circles[i].clicked();
+    }
+  }
+}
+
+function mouseDragged() {
+  circles.push(new circle(mouseX, mouseY));
 }
 
 function updateText() {
