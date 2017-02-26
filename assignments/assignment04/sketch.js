d91e66dc37b827d8eb8f95ad65ed5d2b26eb763e
tiffanyvlee <tiffanyvlee@gmail.com>
2017-02-25 20:24:25 -0500

    edit

--- assignments/assignment04/sketch.js
+++ assignments/assignment04/sketch.js
@@ -1,22 +1,38 @@
-// add sound file (multiple beats?)
-//
-
 var button;
 var textarea;
 var savedText;
 
 function setup() {
-	song = loadSound('');
-	createCanvas(100, 100);
-	background(0);
-
-	noCanvas();
-	textarea = createElement('textarea', 'Enter text here');
-	textarea.size(300, 400);
-	createElement('br');
-	button = createButton('push to save');
-	button.mousePressed(saveText);
+  noCanvas();
+  textarea = createElement('textarea', 'Enter text here!');
+  textarea.size(300,400);
+  createElement('br');
+  button = createButton('push to save');
+  button.mousePressed(saveText);
 }
+
+function saveText() {
+  savedText = textarea.value();
+  createP("ORIGINAL TEXT: " + savedText);
+
+  // get the array of all the words from the savedText variable
+  var textArr = splitTokens(savedText, '.:;?! !@#$%^&*()+');
+  var length = textArr.length;
+
+  // how many elements to remove?
+  var numToRemove = textArr.length/2;
+
+  // loop through the array, pick a random index, remove it from the array
+  for (var i = 0; i < numToRemove; i++) {
+    var whichIndex = int(random(textArr.length));
+    textArr.splice(whichIndex, 1);
+  }
+
+  // join the array into a single string, separated by spaces
+  var newString = join(textArr, ' ');
+  createP("NEW TEXT: " + newString);
+}
+
 
 function mousePressed(){
 	if (song.isPlaying()) {
@@ -24,22 +40,4 @@ function mousePressed(){
 	} else {
 		song.play();
 	}
-}
-
-function saveText() {
-	savedText = textarea.value();
-	createP("original text: " + savedText);
-
-	var textArr = splitTokens(savedText, '.:;?! @#$%()');
-	var length = textArr.length;
-
-	var numToRemove = textArr.length/2;
-
-	for (var i = 0; i < numToRemove; i++) {
-		var whichIndex = int(random(textArr.length));
-		textArr.splice(whichIndex, 1);
-	}
-
-	var newString = join(textArr, ' ');
-	creatP("new text: " + newString);
 }
